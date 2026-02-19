91b2adf22bd9a724183f1fea681159a4071cf6a9
bahadirberkerdemir <bahadirberkerdemir@gmail.com>
2026-02-19 17:48:29 +0300

    Applied CTE and a new Feature

--- 06-Monthly-Dues-In-2013.sql
+++ 06-Monthly-Dues-In-2013.sql
@@ -1,11 +1,19 @@
 USE AdventureWorks2022
 GO
 
-SELECT
- 	MONTH(OrderDate) [Month],
- 	SUM(TotalDue) [Monthly Income]
-FROM Sales.SalesOrderHeader
-WHERE YEAR(OrderDate) = 2013
+WITH MonthlySelling AS (
+    SELECT
+        MONTH(OrderDate) AS [Month],
+        CAST(SUM(TotalDue) AS decimal) AS [Income of This Month],
+        CAST(LAG(SUM(TotalDue), 1) OVER (ORDER BY MONTH(OrderDate)) AS decimal) AS [Income of Prev. Month]
+    FROM Sales.SalesOrderHeader
+    WHERE YEAR(OrderDate) = 2013
+    GROUP BY MONTH(OrderDate)
+)
 
-Group BY MONTH(OrderDate)
-Order BY MONTH(OrderDate);
+SELECT
+    [Month],
+    [Income of This Month],
+    [Income of Prev. Month],
+    CAST(([Income of This Month]-[Income of Prev. Month])*100/[Income of Prev. Month] as nvarchar) + '%' AS [Monthly Change] --New Feature: Monthly Changes
+FROM MonthlySelling;
