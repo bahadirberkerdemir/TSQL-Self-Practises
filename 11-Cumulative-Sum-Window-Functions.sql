f96fe0bc730689064b404470ca362e7f91e7d614
bahadirberkerdemir <bahadirberkerdemir@gmail.com>
2026-02-19 16:55:31 +0300

    Minor Label Edit

--- 11-Cumulative-Sum-Window-Functions.sql
+++ 11-Cumulative-Sum-Window-Functions.sql
@@ -1,10 +1,11 @@
 USE AdventureWorks2022
 GO
 
-SELECT SalesOrderID [Product ID], TotalDue [Total Due],
- 	SUM(TotalDue) OVER(
+SELECT SalesOrderID [Sale ID], TotalDue [Total Due],
+Â 	SUM(TotalDue) OVER(
 
- 	Order BY SalesOrderID
+Â 	Order BY SalesOrderID
 
 ) Cumulative
+
 From Sales.SalesOrderHeader;
